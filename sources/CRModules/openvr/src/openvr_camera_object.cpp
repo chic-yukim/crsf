0e03b62a1f5374e5c6b964c1287703e864c0b63c
Younguk Kim <bluekyu.dev@gmail.com>
2018-11-02 10:56:41 +0900

    Add camera update event

--- sources/CRModules/openvr/src/openvr_camera_object.cpp
+++ sources/CRModules/openvr/src/openvr_camera_object.cpp
@@ -27,12 +27,15 @@
 
 #include <render_pipeline/rpcore/globals.hpp>
 #include <render_pipeline/rppanda/showbase/showbase.hpp>
+#include <render_pipeline/rppanda/showbase/messenger.hpp>
 
 #include <openvr_plugin.hpp>
 #include <openvr_camera_interface.hpp>
 
 #include <crsf/RenderingEngine/TGraphicRenderEngine.h>
 #include <crsf/RenderingEngine/GraphicRenderEngine/TTexture.h>
+
+const char* OpenVRCameraObject::UPDATE_EVENT_NAME = "OpenVRCameraObject::Update";
 
 OpenVRCameraObject::OpenVRCameraObject(OpenVRModule& module_instance) :
 	crsf::TPhysicalCamera(NodePath(module_instance.GetOpenVRPlugin()->get_tracked_camera()->create_camera_node())), module_(module_instance)
@@ -98,6 +101,8 @@ AsyncTask::DoneStatus OpenVRCameraObject::Update(rppanda::FunctionalTask* task)
 	}
 
 	frame_header_ = new_frame_header;
+
+    rpcore::Globals::base->get_messenger()->send("OpenVRCameraObject::Update", false);
 
 	return AsyncTask::DS_cont;
 }
